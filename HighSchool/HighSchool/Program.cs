23929132a340f8e0620fb513ee7baea4220c41d9
nerdelt <nerdelt@gmail.com>
2018-10-03 00:54:15 -0500

    Assignment 13-2 HighSchool

---
+++ HighSchool/HighSchool/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace HighSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List of students with name, gpa, and grade
+            List<Student> allStudents = new List<Student>() {
+             new Student {FirstName = "Marjorie", LastName = "Boucher", GPA = 3.2M, Grade = 11 },
+             new Student {FirstName = "Howard", LastName = "Newell", GPA = 2.8M, Grade = 8},
+             new Student {FirstName = "Norma", LastName = "Childs", GPA = 3.8M, Grade = 9 },
+             new Student {FirstName = "Ruby", LastName = "Ibarra", GPA = 2.4M, Grade = 7 },
+             new Student {FirstName = "Joyce", LastName = "Winter", GPA = 4.0M, Grade = 10},
+             new Student {FirstName = "Francis", LastName = "Davies", GPA = 3.7M, Grade = 6},
+             new Student {FirstName = "Melvin", LastName = "Schultz", GPA = 3.9M, Grade = 9},
+             new Student {FirstName = "Edith", LastName = "Gibbons", GPA = 3.0M, Grade = 8},
+             new Student {FirstName = "Clyde", LastName = "Westridge", GPA = 4.0M, Grade = 12},
+             new Student {FirstName = "Agnes", LastName = "Delarosa", GPA = 3.5M, Grade = 7},
+             new Student {FirstName = "Gilbert", LastName = "Kessler", GPA = 2.9M, Grade = 5},
+             new Student {FirstName = "Clara", LastName = "Downey", GPA = 3.2M, Grade = 6},
+             new Student {FirstName = "Russell", LastName = "Espinosa", GPA = 3.6M, Grade = 8},
+             new Student {FirstName = "Florence", LastName = "Vogel", GPA = 3.8M, Grade = 5}
+            };
+
+            Console.WriteLine($"The following students are on the High School's Honor Roll.\n");
+
+            //calls the method from the studnet class
+            Student.GetHonorRoll(allStudents);
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
---
+++ HighSchool/HighSchool/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HighSchool
+{
+    class Student
+    {
+        //all the properties needed to identify student 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal GPA { get; set; }
+        public int Grade { get; set; }
+
+        public static void GetHonorRoll(List<Student> studentsList)
+        {
+            //LINQ query 
+            var honorRoll = studentsList.Where(s => s.GPA >= 3.0M && (s.Grade >= 9 && s.Grade <= 12));
+
+
+            var highGrade = honorRoll.OrderByDescending(g => g.Grade).ThenByDescending(g => g.GPA).ThenBy(g => g.LastName).ThenBy(g => g.FirstName);
+
+            //printing out honor roll students 
+            foreach (var student in highGrade)
+            {
+                Console.WriteLine($"Name: {student.FirstName} {student.LastName}, GPA: {student.GPA}, Grade: {student.Grade}.\n");
+                
+            }
+             
+        }  
+
+    }
+}
